746a5b2a9ac5aedf7b3d6a57657a39c918a48572
Charx19 <105243261+Charx19@users.noreply.github.com>
2024-01-28 12:41:51 +0100

    Add files via upload

---
+++ T.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+
+gnuplot << EOF
+	
+	set datafile separator ";"
+	set terminal pngcairo enhanced font 'arial,12' size 1200,1000
+	set output "traitementT.png"
+	set title "Option T- The top 10 travelled towns" 
+	set title offset 5,0
+	set style data histograms
+	set style histogram clustered
+	set style fill solid border -1
+	set xtic offset 0,-6 
+	set yrange [0:4000]
+	set ylabel " NB routes " 
+	set xlabel " Town Names" offset 0,-8 
+	plot 'dataT.txt' using 2:xtic(1) title "Total routes",\
+	     'dataT.txt' using 3 title "First Town"
+	
+	$data
+
+EOF
+
+xdg-open "traitementT.png" & 
