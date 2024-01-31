4a10d98755c6f635d632f9dbe95385da28900800
Abishek Suresh <abhisheksuresh0801@gmail.com>
2024-01-31 23:23:58 +0100

    Changes

--- progc/main.c
+++ progc/main.c
@@ -3,7 +3,23 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "city.h"
+#include "route.h"
+#include "utils.h"
+
+
 int main(int argc, char **argv)
 {
+    // Traitement des données du fichier
+    FILE* inputFile = fopenSafe(argv[1], "r"); // "../data/data.csv"
 
+    FILE* outputFile = fopenSafe(argv[2], "w");
+    
+    if(strcmp(argv[3], "-t") == 0){
+        sortT(inputFile, outputFile);
+    } else if (strcmp(argv[3], "-s") == 0){
+        sortS(inputFile, outputFile);
+    }
+
+    return 0;
 }
